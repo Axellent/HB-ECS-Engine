0ff5cd454e5e4c85246116de4e9af49718f9cf8c
solarstrings <solar.strings@yahoo.se>
2016-04-04 16:07:45 +0200

    Added modelclass and modelsystem

--- ECS Engine/Source/Components/CameraComponent.cs
+++ ECS Engine/Source/Components/CameraComponent.cs
@@ -9,29 +9,25 @@ namespace ECS_Engine.Source.Components
 {
     class CameraComponent
     {
-        public float nearClipPlane { get; set; }
-        public float farClipPlane { get; set; }
+        public float nearClipPlane = 1f;
+        public float farClipPlane = 1000f;
         public float aspectRatio { get; set; }
 
         public float fieldOfView { get; set; }
 
-        public Vector3 upVector { get; set; }
         public Vector3 position { get; set; }
         public Vector3 direction { get; set; }
         public Vector3 rotation { get; set; }
 
-
-        public CameraComponent(GraphicsDeviceManager graphics)
+        public Matrix viewMatrix;
+        public Matrix projectionMatrix;
+        
+        public CameraComponent(GraphicsDeviceManager graphics, Vector3 position, Vector3 direction, Vector3 movement)
         {
             aspectRatio = graphics.PreferredBackBufferWidth / (float)graphics.PreferredBackBufferHeight;
-            farClipPlane = 500;
-            nearClipPlane = 1;
             fieldOfView = MathHelper.PiOver4;
-        }
-        
-        void test()
-        {
-            Matrix.create
+            viewMatrix = Matrix.CreateLookAt(position, direction, Vector3.Up);
+            Matrix.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearClipPlane, farClipPlane, out projectionMatrix);
         }
 
         public void LookAt(ref CameraPosition)
@@ -39,7 +35,30 @@ namespace ECS_Engine.Source.Components
 
         }
 
+        public void SetNearClipPlane(float value)
+        {
+            if(value<=0) {
+                this.nearClipPlane = 1f;
+            }
 
+            this.nearClipPlane = value;
+        }
+
+        public void SetFarClipPlane(float value)
+        {
+            if (value >= 10000)
+            {
+                this.farClipPlane = 10000;
+            }
+            else if (value < 50)
+            {
+                this.farClipPlane = 50;
+            }
+            else
+            {
+                this.farClipPlane = value;
+            }
+        }
 
     }
 }
