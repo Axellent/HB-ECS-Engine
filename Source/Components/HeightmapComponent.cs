183fba198ad3d95b04b6097c2349db514934aac8
Axellent <axel.sigl@gmail.com>
2016-04-10 20:21:47 +0100

    Heightmap fully implemented

--- Source/Components/HeightmapComponent.cs
+++ Source/Components/HeightmapComponent.cs
@@ -10,9 +10,40 @@ namespace GameEngine
 {
     public class HeightmapComponent : IComponent
     {
-        List<VertexPositionColor> vertices;
-        float[,] heightData;
-        public List<short> vertexBuffer { get; set; }
+        public GraphicsDevice graphicsDevice;
+        public BasicEffect effect;
+        public Texture2D heightmapTexture;
+        public Matrix viewMatrix;
+        public Matrix projectionMatrix;
 
+        public VertexBuffer vertexBuffer;
+        public IndexBuffer indexBuffer;
+
+        public int terrainWidth = 4;
+        public int terrainHeight = 3;
+        public float indicesLenDiv3 = 1;
+        public float angle = 0;
+
+        public HeightmapComponent(GraphicsDevice graphicsDevice, BasicEffect effect, Texture2D texture)
+        {
+            this.graphicsDevice = graphicsDevice;
+            this.effect = effect;
+            heightmapTexture = texture;
+        }
+
+        public void SetMatrices(Matrix viewMatrix, Matrix projectionMatrix)
+        {
+            this.viewMatrix = viewMatrix;
+            this.projectionMatrix = projectionMatrix;
+        }
+
+        public void UpdateHeightmap(VertexBuffer vertexBuffer, IndexBuffer indexBuffer, int terrainWidth, int terrainHeight, float indicesLenDiv3)
+        {
+            this.vertexBuffer = vertexBuffer;
+            this.indexBuffer = indexBuffer;
+            this.terrainWidth = terrainWidth;
+            this.terrainHeight = terrainHeight;
+            this.indicesLenDiv3 = indicesLenDiv3;
+        }
     }
 }
--- Source/Systems/HeightmapSystem.cs
+++ Source/Systems/HeightmapSystem.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GameEngine
 {
-    public class HeightmapSystem
+    public class HeightmapSystem : IRenderSystem
     {
         public struct VertexPositionColorNormal
         {
@@ -23,11 +23,32 @@ namespace GameEngine
             );
         }
 
-        public float[,] GetHeightData(Texture2D heightMap)
+        public void Render(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            Entity heightMapEntity = ComponentManager.Instance.GetFirstComponentOfType<HeightmapComponent>();
+            HeightmapComponent heightmapComponent = ComponentManager.Instance.GetEntityComponent<HeightmapComponent>(heightMapEntity);
+
+            Matrix worldMatrix = Matrix.CreateTranslation(-heightmapComponent.terrainWidth / 2.0f, 0, heightmapComponent.terrainHeight / 2.0f) * Matrix.CreateRotationY(heightmapComponent.angle);
+            heightmapComponent.effect.View = heightmapComponent.viewMatrix;
+            heightmapComponent.effect.Projection = heightmapComponent.projectionMatrix;
+            heightmapComponent.effect.World = worldMatrix;
+            heightmapComponent.effect.VertexColorEnabled = true;
+
+            foreach (EffectPass pass in heightmapComponent.effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                heightmapComponent.graphicsDevice.Indices = heightmapComponent.indexBuffer;
+                heightmapComponent.graphicsDevice.SetVertexBuffer(heightmapComponent.vertexBuffer);
+                heightmapComponent.graphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, (int)heightmapComponent.indicesLenDiv3);
+            }
+        }
+
+        public static float[,] GetHeightData(Texture2D heightMap, ref int terrainWidth, ref int terrainHeight)
         {
             float[,] heightData;
-            int terrainWidth = heightMap.Width;
-            int terrainHeight = heightMap.Height;
+            terrainWidth = heightMap.Width;
+            terrainHeight = heightMap.Height;
 
             Color[] heightMapColors = new Color[terrainWidth * terrainHeight];
             heightMap.GetData(heightMapColors);
@@ -43,7 +64,7 @@ namespace GameEngine
             return heightData;
         }
 
-        public int[] GetIndices(int terrainWidth, int terrainHeight)
+        public static int[] GetIndices(int terrainWidth, int terrainHeight)
         {
             int[] indices = new int[(terrainWidth - 1) * (terrainHeight - 1) * 6];
             int counter = 0;
@@ -69,11 +90,11 @@ namespace GameEngine
             return indices;
         }
 
-        public VertexPositionColor[] GetVertices(int terrainWidth, int terrainHeight, float[,] heightData)
+        public static VertexPositionColorNormal[] GetVertices(int terrainWidth, int terrainHeight, float[,] heightData)
         {
             float minHeight = float.MaxValue;
             float maxHeight = float.MinValue;
-            VertexPositionColor[] vertices = new VertexPositionColor[terrainWidth * terrainHeight];
+            VertexPositionColorNormal[] vertices = new VertexPositionColorNormal[terrainWidth * terrainHeight];
 
             FindExtremeHeights(ref minHeight, ref maxHeight, terrainWidth, terrainHeight, heightData);
 
@@ -96,7 +117,7 @@ namespace GameEngine
             return vertices;
         }
 
-        public void TransformToNormals(ref VertexPositionColorNormal[] vertices, int[] indices)
+        public static void TransformToNormals(ref VertexPositionColorNormal[] vertices, int[] indices)
         {
             for (int i = 0; i < vertices.Length; i++)
                 vertices[i].Normal = new Vector3(0, 0, 0);
@@ -120,7 +141,16 @@ namespace GameEngine
                 vertices[i].Normal.Normalize();
         }
 
-        private void FindExtremeHeights(ref float minHeight, ref float maxHeight, int terrainWidth, int terrainHeight, float[,] heightData)
+        public static void CopyToBuffers(ref VertexBuffer vertexBuffer, ref IndexBuffer indexBuffer, VertexPositionColorNormal[] vertices, int[] indices, GraphicsDevice graphicsDevice)
+        {
+            vertexBuffer = new VertexBuffer(graphicsDevice, VertexPositionColorNormal.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+            vertexBuffer.SetData(vertices);
+
+            indexBuffer = new IndexBuffer(graphicsDevice, typeof(int), indices.Length, BufferUsage.WriteOnly);
+            indexBuffer.SetData(indices);
+        }
+
+        private static void FindExtremeHeights(ref float minHeight, ref float maxHeight, int terrainWidth, int terrainHeight, float[,] heightData)
         {
             for (int x = 0; x < terrainWidth; x++)
             {
